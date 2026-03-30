e6644c6ddd061fba6a14df8bf88e9f83cff0ee23
Monica Devassy <monicadevassy@gmail.com>
2026-03-30 16:42:35 +0530

    Add files via upload

---
+++ Assignments/assigment1.py
@@ -0,0 +1,22 @@
+#----------------------------------------------------recieving inputs
+x=float(input("enter the first number: "))
+y=float(input("enter the second number: "))
+
+#----------------------------------------------------printing after doing arithmetic operations
+print("the value after addition is :",x+y)
+print("the value after subtraction is :",x-y)
+print("the value after multiplication is :",x*y)
+print("the value after division is :",x/y)
+
+#----------------------------------------------------changing type of inputs
+x=str(x)
+y=str(y)
+
+#---------------------------------------------------concatinating the inputs
+ans=x+y
+
+#---------------------------------------------------printing the final string
+print(ans)
+
+
+
